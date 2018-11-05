b125cc8ecb8fd81ed7671bb3530d7a2273d76a6b
Blanche Berneron <blanche.berneron@epfl.ch>
2018-11-05 22:26:37 +0100

    Final version

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -9,7 +9,7 @@ void Simulation::initialize(int argc, char **argv) {
     TCLAP::CmdLine cmd("Network simulation");
     TCLAP::ValueArg<int> argNode("n", "nodes", "Number of nodes", false, 0, "int");
     cmd.add(argNode);
-    TCLAP::ValueArg<long> argSeed("S", "seed", "Random seed", false, 0, "long");
+    TCLAP::ValueArg<long> argSeed("S", "seed", "Random seed", false, 0, "long"); //?
     cmd.add(argSeed);
     TCLAP::ValueArg<double> argDegree("d", "degree", "Mean node degree", false, 4, "double");
     cmd.add(argDegree);
@@ -19,12 +19,12 @@ void Simulation::initialize(int argc, char **argv) {
     cmd.parse(argc, argv);
 
     RNG.initialize(argSeed.getValue());
-    int nodes(argNode.getValue());
-    if (nodes < 1) nodes = RNG.poisson(50);
-    _network->resize(nodes);
-    double degree(argDegree.getValue());
-    if (degree < 1) degree = RNG.uniform_double(1, std::sqrt(nodes));
-    size_t nlink = _network->random_connect(degree);
+    int nodes(argNode.getValue());       //nombree de nodes
+    if (nodes < 1) nodes = RNG.poisson(50);   // si nous avons pas de nodes on  lui donne un nombre aleatoire grace à l'utilisation la loi de poisson
+    _network->resize(nodes);                 //Resizes the list of nodes
+    double degree(argDegree.getValue());       // stock la valeur donné par le arg dans une nouvelle variable degree
+    if (degree < 1) degree = RNG.uniform_double(1, std::sqrt(nodes));  // si nous avons rien dans cette valeur on lui donne une valleur aléatoire grace à la loi uniform
+    size_t nlink = _network->random_connect(degree);   //on donne a la variable nlink (nombre de link total) --> degree: nombre de liens en moyenne par noodle  de la network
     std::cout << _network->size() << " nodes, " << nlink << " links\n";
     maxtime = argTime.getValue();
 }
@@ -46,11 +46,11 @@ void Simulation::print(const int &time) const {
 }
 
 void Simulation::step() const {
-    std::vector<double> next_values(_network->size());
-    std::vector<double> epsilon(_network->size());
-    RNG.uniform_double(epsilon);
-    for (size_t node=0; node<next_values.size(); node++) {
-        double cumul_neighbs = 0;
+    std::vector<double> next_values(_network->size());  //meme taille que values
+    std::vector<double> epsilon(_network->size());    //la valeur suivante doit differer d'epsilon
+    RNG.uniform_double(epsilon);     //remplie le tableau avec des nombres aleatooires qui suivent la loi uniforme
+    for (size_t node=0; node<next_values.size(); node++) {  //remplir le tableau de next value avec des valeurs qui suivent un calcul savant
+        double cumul_neighbs = 0;   //cumulateur des valeurs de noodle courrante
         for (auto I : _network->neighbors(node)) cumul_neighbs += _network->value(I);
         double ddeg(_network->degree(node));
         next_values[node] = epsilon[node]*cumul_neighbs/ddeg+(1-epsilon[node])*_network->value(node);
